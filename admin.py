f3eb5eb6d8287ad50e34ebad5b015358fab28b5c
idergunoff <idergunoff@yandex.ru>
2022-11-27 22:12:23 +0300

    админка

--- admin.py
+++ admin.py
@@ -1,7 +1,64 @@
 from func.admin_func import *
+from func.task_list_func import create_show_tasks_kb, create_show_tasks_mes
 
 
 @dp.callback_query_handler(cb_admins.filter())
 @logger.catch
 async def show_admins(call: types.CallbackQuery, callback_data: dict):
-    chat = get_chat(callback_data['chat_id'])
+    chat = await get_chat(callback_data['chat_id'])
+    mes = await create_mes_admins(chat)
+    kb_admin = await create_kb_admins(chat)
+    await call.message.edit_text(mes, reply_markup=kb_admin)
+    await call.answer()
+
+
+@dp.callback_query_handler(cb_back_task_admin.filter())
+@logger.catch
+async def back_task_admin(call: types.CallbackQuery, callback_data: dict):
+    user = await get_user(call.from_user.id)
+    chat = await get_chat(callback_data['chat_id'])
+    kb_task = await create_show_tasks_kb(chat, 0, task_list=user.task_list)
+    if user.super_admin:
+        kb_task.insert(InlineKeyboardButton('Админы',
+                                            callback_data=cb_admins.new(chat_id=chat.chat_id)))
+    mes = await create_show_tasks_mes(chat, call.from_user.id, 0, task_list=user.task_list)
+    await call.message.edit_text(mes, reply_markup=kb_task)
+    await call.answer()
+
+
+@dp.callback_query_handler(cb_add_admin.filter())
+@logger.catch
+async def add_admin(call: types.CallbackQuery, callback_data: dict):
+    chat = await get_chat(callback_data['chat_id'])
+    kb_add_admin = await create_kb_add_admins(chat)
+    await call.message.edit_text(f'Выберите админа чата <b>{chat.title}</b> из пользователей:', reply_markup=kb_add_admin)
+    await call.answer()
+
+
+@dp.callback_query_handler(cb_del_admin.filter())
+@logger.catch
+async def del_admin(call: types.CallbackQuery, callback_data: dict):
+    chat = await get_chat(callback_data['chat_id'])
+    kb_del_admin = await create_kb_del_admins(chat)
+    await call.message.edit_text(f'Выберите пользователя чата <b>{chat.title}</b> для удаления из админов:', reply_markup=kb_del_admin)
+    await call.answer()
+
+
+@dp.callback_query_handler(cb_back_admins.filter())
+@logger.catch
+async def back_admin(call: types.CallbackQuery, callback_data: dict):
+    await show_admins(call, callback_data)
+
+
+@dp.callback_query_handler(cb_add_user_admin.filter())
+@logger.catch
+async def add_user_admin(call: types.CallbackQuery, callback_data: dict):
+    await add_user_admin_db(callback_data['chat_id'], callback_data['user_id'])
+    await show_admins(call, callback_data)
+
+
+@dp.callback_query_handler(cb_del_user_admin.filter())
+@logger.catch
+async def del_user_admin(call: types.CallbackQuery, callback_data: dict):
+    await del_user_admin_db(callback_data['chat_id'], callback_data['user_id'])
+    await show_admins(call, callback_data)
--- func/admin_func.py
+++ func/admin_func.py
@@ -6,10 +6,50 @@ from func.function import *
 @logger.catch
 async def create_mes_admins(chat):
     mes = f'Администраторы чата <b>{chat.title}</b>:'
-    for i in session.query(Participant).filter(Participant.chat_id == chat.chat_id, Participant.admin == True).all():
-        mes += f'\n{i.user.name}'
+    for n, i in enumerate(session.query(Participant).filter(Participant.chat_id == chat.chat_id, Participant.admin == True).all()):
+        mes += f'\n{str(n + 1)}. {i.user.name}'
     return mes
 
 
+@logger.catch
 async def create_kb_admins(chat):
+    kb_admin = InlineKeyboardMarkup(row_width=2)
+    btn_add_admin = InlineKeyboardButton('Назначить', callback_data=cb_add_admin.new(chat_id=chat.chat_id))
+    btn_del_admin = InlineKeyboardButton('Убрать', callback_data=cb_del_admin.new(chat_id=chat.chat_id))
+    btn_back_task_admin = InlineKeyboardButton('Выйти из админ-меню', callback_data=cb_back_task_admin.new(chat_id=chat.chat_id))
+    kb_admin.row(btn_add_admin, btn_del_admin).row(btn_back_task_admin)
+    return kb_admin
 
+
+@logger.catch
+async def create_kb_add_admins(chat):
+    kb_add_admin = InlineKeyboardMarkup(row_width=1)
+    for i in session.query(Participant).filter(Participant.chat_id == chat.chat_id, Participant.admin == False).all():
+         kb_add_admin.insert(InlineKeyboardButton(f'{i.user.name}', callback_data = cb_add_user_admin.new(chat_id=chat.chat_id, user_id=i.user.t_id)))
+    btn_back_admin = InlineKeyboardButton('Отмена', callback_data=cb_back_admins.new(chat_id=chat.chat_id))
+    kb_add_admin.insert(btn_back_admin)
+    return kb_add_admin
+
+
+@logger.catch
+async def create_kb_del_admins(chat):
+    kb_del_admin = InlineKeyboardMarkup(row_width=1)
+    for i in session.query(Participant).filter(Participant.chat_id == chat.chat_id, Participant.admin == True).all():
+         kb_del_admin.insert(InlineKeyboardButton(f'{i.user.name}', callback_data = cb_del_user_admin.new(chat_id=chat.chat_id, user_id=i.user.t_id)))
+    btn_back_admin = InlineKeyboardButton('Отмена', callback_data=cb_back_admins.new(chat_id=chat.chat_id))
+    kb_del_admin.insert(btn_back_admin)
+    return kb_del_admin
+
+
+@logger.catch
+async def add_user_admin_db(chat_id, user_id):
+    session.query(Participant).filter(Participant.chat_id == chat_id, Participant.user_id == user_id).update(
+        {'admin': True}, synchronize_session='fetch')
+    session.commit()
+
+
+@logger.catch
+async def del_user_admin_db(chat_id, user_id):
+    session.query(Participant).filter(Participant.chat_id == chat_id, Participant.user_id == user_id).update(
+        {'admin': False}, synchronize_session='fetch')
+    session.commit()
